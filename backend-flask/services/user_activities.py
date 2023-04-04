390572e17b366a2c16118d1c0fb177c4e2a49225
Angel Rojas <angelrojas691@hotmail.com>
2023-04-04 19:58:50 +0000

    Week 7 - FargateConfiguring for Container Insights

--- backend-flask/services/user_activities.py
+++ backend-flask/services/user_activities.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timedelta, timezone
-from aws_xray_sdk.core import xray_recorder
+#from aws_xray_sdk.core import xray_recorder
 class UserActivities:
   def run(user_handle):
     try:
@@ -22,16 +22,17 @@ class UserActivities:
           'expires_at': (now + timedelta(days=31)).isoformat()
         }]
         model['data'] = results
+        
 
-      subsegment = xray_recorder.begin_subsegment('mock-data')
-      # xray ---
-      dict = {
-        "now": now.isoformat(),
-        "results-size": len(model['data'])
-      }
-      subsegment.put_metadata('key', dict, 'namespace')
-      xray_recorder.end_subsegment()
+      #subsegment = xray_recorder.begin_subsegment('mock-data')
+      ## xray ---
+      #dict = {
+      #  "now": now.isoformat(),
+      #  "results-size": len(model['data'])
+      #}
+      #subsegment.put_metadata('key', dict, 'namespace')
+      #xray_recorder.end_subsegment()
     finally:  
-    #  # Close the segment
-      xray_recorder.end_subsegment()
-    return model
+    ##  # Close the segment
+    #  xray_recorder.end_subsegment()
+      return model
